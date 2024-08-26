3b07a97c3542ea495cb065e72e0bc4360fe2f249
Krier <vinoshan@outlook.com>
2024-08-25 21:25:11 -0400

    Added Script with Local Storage Feature

---
+++ script.js
@@ -0,0 +1,42 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Please enter a task");
+  } else {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = inputBox.value;
+    listContainer.appendChild(listItem);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00D7";
+    listItem.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    }
+    if (e.target.tagName === "SPAN") {
+      e.target.parentElement.style.display = "none";
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
